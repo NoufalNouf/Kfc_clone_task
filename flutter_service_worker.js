de63e3b440e0b113c16376a57233dcd225ceb16c
NoufalNouf <noufalnoufal44330@gmail.com>
2025-12-09 04:30:02 +0530

    updated web ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e30487191824c6f5b154eeac5cea80a3
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2d1453d2879fdefcb2ae315e2537dffb",
+"flutter_bootstrap.js": "8e4ba0207c93bef6f681e3a3d37c63ae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
